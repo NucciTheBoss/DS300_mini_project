62549bf9bb6f6f0e3ace1bd7f04d51e37d4b6900
Jason Nucciarone <nucci.programming@gmail.com>
2021-12-05 17:19:29 -0500

    Finish dfserver

--- dfserver.py
+++ dfserver.py
@@ -6,7 +6,6 @@ import sqlite3
 from flask import Flask, jsonify
 from typing import Any, List, Union
 
-
 # Global variables
 app = Flask(__name__)
 DF = None
@@ -29,9 +28,10 @@ class DBManager:
     """
 
     _class_instance = None
-    _db_instance = False
     _connection = None
     _cursor = None
+    _db_info = None
+    _db_instance = False
 
     def __init__(self) -> None:
         """ Private constructor. """
@@ -59,6 +59,7 @@ class DBManager:
         Create ephemeral SQL database in RAM. Database goes away after program ends.
 
         :param path_to_csv: Path to CSV file.
+        :return: Dictionary with basic info on DB.
         """
         if DBManager._db_instance is False:
             # Create DB in RAM and cursor to navigate database
@@ -66,8 +67,15 @@ class DBManager:
             DBManager._cursor = DBManager._connection.cursor()
 
             # Read in csv file and add to the DB
-            driving_data = pd.read_csv(path_to_csv)
-            driving_data.to_sql("PPP", DBManager._connection, if_exists="replace")
+            ppp_data = pd.read_csv(path_to_csv)
+            data_info = {
+                "attr_names": [col for col in ppp_data.columns],
+                "columns": ppp_data.shape[1],
+                "rows": ppp_data.shape[0],
+                "missing_values": False
+            }
+            DBManager._db_info = data_info
+            ppp_data.to_sql("PPP", DBManager._connection, if_exists="replace")
 
             # Set to true so that we do not run block more than once
             DBManager._db_instance = True
@@ -81,6 +89,14 @@ class DBManager:
         """
         return self._cursor.execute(parse(query)).fetchall()
 
+    def get_db_info(self) -> dict:
+        """
+        Return info on database.
+
+        :return: Dictionary containing database info.
+        """
+        return self._db_info
+
 
 class DFManager:
     """
@@ -90,6 +106,7 @@ class DFManager:
     - Using laplace mechanism to keep things straightforward.
     - Only querying one attribute.
     """
+
     def __init__(self) -> None:
         """ Constructor. Shut off access after 100 queries to not spill the beans. """
         self.allowable_number_of_queries = 100
@@ -97,7 +114,7 @@ class DFManager:
         self.epsilon = 0.1
 
     @staticmethod
-    def _calculate_sensitivity_single(query: str) -> Union[int, float, None]:
+    def _calculate_sensitivity(query: str) -> Union[int, float, None]:
         """
         Calculate the sensitivity of a query.
 
@@ -115,7 +132,8 @@ class DFManager:
             temp_index = query_as_list.index("FROM")
 
             # Get attribute we want to find the MAX and MIN for
-            attr = query_as_list[temp_index:][0]
+            attr = query_as_list[:temp_index][-1]
+            attr = attr[attr.find("(")+1:attr.find(")")]
 
             # Get max and min and return result of max - min
             max_result = DBManager.get_instance().execute(f"SELECT+MAX({attr})+FROM+PPP")
@@ -130,18 +148,20 @@ class DFManager:
             temp_index = query_as_list.index("FROM")
 
             # Get the attribute we want to find the SUM for
-            attr = query_as_list[temp_index:][0]
+            attr = query_as_list[:temp_index][-1]
+            attr = attr[attr.find("(")+1:attr.find(")")]
 
             # Get sum and count and return result of sum / count
             sum_result = DBManager.get_instance().execute(f"SELECT+SUM({attr})+FROM+PPP")
             count_result = DBManager.get_instance().execute("SELECT+COUNT(*)+FROM+PPP")
 
-            return sum_result[0][0] - count_result[0][0]
+            return sum_result[0][0] / count_result[0][0]
 
         else:
             return None
 
-    def add_noise(self, query: str, result: Union[int, float]) -> Union[int, float, None]:
+    def add_noise(self, query: str, result: Union[int, float, List]) -> Union[
+            List[List[Union[float, Any]]], float, None]:
         """
         Add noise to a single value.
 
@@ -150,63 +170,83 @@ class DFManager:
         :return: Noisy result.
         """
         if self.number_of_queries_executed < self.allowable_number_of_queries:
-            sensitivity = DFManager._calculate_sensitivity_single(query)
-            noisy_result = self._apply_laplace_mechanism(result, sensitivity)
-            self.number_of_queries_executed += 1
-            return noisy_result
+            if isinstance(result, list):
+                sensitivity = DFManager._calculate_sensitivity(query)
+                noisy_result = map(lambda x: [x[0], self._apply_laplace_mechanism(x[1], sensitivity)], result)
+                self.number_of_queries_executed += 1
+                return list(noisy_result)
 
-        else:
-            return None
-
-    def add_noise_parallel(self, query: str, result: List[Union[int, float]]) -> Union[List[Union[int, float]], None]:
-        """
-        Add noise to an array. Not using pd.Series because cannot be transferred over the network.
-
-        :param query: Query executed on dataset.
-        :param result: Original result.
-        :return: Noisy array.
-        """
-        if self.number_of_queries_executed < self.allowable_number_of_queries:
-            # TODO: Write out this method after verifying the single DP works
-            self.number_of_queries_executed += 1
+            else:
+                sensitivity = DFManager._calculate_sensitivity(query)
+                noisy_result = self._apply_laplace_mechanism(result, sensitivity)
+                self.number_of_queries_executed += 1
+                return noisy_result
 
         else:
             return None
 
     def _apply_laplace_mechanism(self, value: Union[int, float], sensitivity: Union[int, float]) -> float:
         """
-        Add noise to original value using the laplace mechanism
+        Add noise to original value using the laplace mechanism.
 
         :param value: Value to add noise to.
         :param sensitivity: Sensitivity of the query.
         :return: Noisy result.
         """
-        return value + np.random.laplace(loc=0, scale=sensitivity/self.epsilon)
+        return value + np.random.laplace(loc=0, scale=sensitivity / self.epsilon)
+
+
+# URL route to retrieve information on SQLite database
+@app.route("/info", methods=["GET"])
+def info():
+    print("Received request for information on database.")
+    return jsonify(DBManager.get_instance().get_db_info())
 
 
 # URL route to use if we want to return the original query result
 @app.route("/orig/<query>", methods=["GET"])
 def orig(query):
-    print(f"Received request for non-noisy answer for query {parse(query)}")
+    print(f"Received request for non-noisy answer for query `{parse(query)}`.")
     result = DBManager.get_instance().execute(query)
 
-    return jsonify({
-        "query": parse(query),
-        "result": result[0][0]
-    })
+    # If query is just looking for a single number
+    if len(result) == 1:
+        return jsonify({
+            "query": parse(query),
+            "result": result[0][0]
+        })
+
+    # If query result contains more than 1 row
+    else:
+        return jsonify({
+            "query": parse(query),
+            "result": [row for row in result]
+        })
 
 
 # URL route to use if we want to return the differential private query result
 @app.route("/diff/<query>", methods=["GET"])
 def diff(query):
-    print(f"Received request for noisy answer for query {parse(query)}")
+    print(f"Received request for noisy answer for query `{parse(query)}`.")
     orig_result = DBManager.get_instance().execute(query)
-    noisy_result = DF.add_noise(query, orig_result[0][0])
 
-    return jsonify({
-        "query": parse(query),
-        "result": noisy_result
-    })
+    # If query is just looking for a single number
+    if len(orig_result) == 1:
+        noisy_result = DF.add_noise(query, orig_result[0][0])
+
+        return jsonify({
+            "query": parse(query),
+            "result": noisy_result
+        })
+
+    # If query result contains more than 1 row
+    else:
+        noisy_result = DF.add_noise(query, [row for row in orig_result])
+
+        return jsonify({
+            "query": parse(query),
+            "result": noisy_result
+        })
 
 
 if __name__ == "__main__":
